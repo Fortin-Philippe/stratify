6438dc605e879cb82d9509df976bd33094719917
Fortin-Philippe <159812164+Fortin-Philippe@users.noreply.github.com>
2025-09-23 20:37:59 -0400

    <feat>:<Ajout de la création d'un utilisateur>

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+from bd import ajouter_utilisateur
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("accueil.html")
+        return render_template("accueil.html")
+
+@app.route('/creer-utilisateur', methods=['GET', 'POST'])
+def form_utilisateur():
+    if request.method == 'POST':
+        nom_utilisateur = request.form['nom_utilisateur']
+        courriel = request.form['courriel']
+        mdp = request.form['mdp']
+        description = request.form.get('description', None)
+        est_coach = bool(request.form.get('est_coach'))
+
+        utilisateur = {
+            "nom_utilisateur": nom_utilisateur,
+            "courriel": courriel,
+            "mdp": mdp,
+            "description": description,
+            "est_coach": est_coach
+        }
+
+        ajouter_utilisateur(utilisateur)
+
+        return redirect(url_for('home'))
+    else:
+         return render_template("form-utilisateur.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
